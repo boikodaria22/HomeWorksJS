60bcc53a5b1b48f439e0b6fa1b00c4de5d761725
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 22:12:56 +0300

    Update HomeWorkJS_2.1.js

--- JS_HomeWorks/js/HomeWork_2/HomeWorkJS_2.1.js
+++ JS_HomeWorks/js/HomeWork_2/HomeWorkJS_2.1.js
@@ -1,11 +1,11 @@
 var user = {
-	name: 'Дарья',
-	sayHello: function(wellcome = 'Добрый день') {
-		if (typeof(this.name) === 'string' && typeof(this.name) !== undefined && this.name.length !== 0) {
-			return (`${wellcome}, ${this.name}!`);
-		} else {
-			return ('Имя пользователя не указано');
-		}
-	}
+  name: 'Дарья',
+  sayHello: function(wellcome = 'Добрый день') {
+    if (typeof(this.name) === 'string' && typeof(this.name) !== undefined && this.name.length !== 0) {
+      return (`${wellcome}, ${this.name}!`);
+    } else {
+      return ('Имя пользователя не указано');
+    }
+  }
 };
 user.sayHello('Привет');
