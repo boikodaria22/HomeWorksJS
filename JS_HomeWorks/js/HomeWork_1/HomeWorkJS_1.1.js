34968d79fa67ccdcc23822ccde814270207252e4
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 22:10:45 +0300

    Update HomeWorkJS_1.1.js

--- JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.1.js
+++ JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.1.js
@@ -1,51 +1,43 @@
 //for
 var array = [16, true, 'MetraBit', NaN, {
-	name: 'UserName',
-	hobby: 'JS'
+  name: 'UserName',
+  hobby: 'JS'
 }, 30, NaN, null, false, 'Vue'];
-
 for (var i = 0; i < array.length; i++) {
-	if (isNaN(array[i])) {
-		array[i] = null;
-	}
+  if (isNaN(array[i])) {
+    array[i] = null;
+  }
 }
-
 console.log('Проверка и изменение массива завершена');
 console.log(array);
-
 //while
 var array = [16, true, 'MetraBit', NaN, {
-	name: 'UserName',
-	hobby: 'JS'
+  name: 'UserName',
+  hobby: 'JS'
 }, 30, NaN, null, false, 'Vue'];
-
 var i = 0;
 while (i < array.length) {
-	if (isNaN(array[i])) {
-		array[i] = null;
-	}
-	i++;
+  if (isNaN(array[i])) {
+    array[i] = null;
+  }
+  i++;
 }
-
 console.log('Проверка и изменение массива завершена');
 console.log(array);
-
 //do...while
 var array = [16, true, 'MetraBit', NaN, {
-	name: 'UserName',
-	hobby: 'JS'
+  name: 'UserName',
+  hobby: 'JS'
 }, 30, NaN, null, false, 'Vue'];
-
 var i = 0;
 do {
-	{
-		if (isNaN(array[i])) {
-			array[i] = null;
-		}
-		i++;
-	}
+  {
+    if (isNaN(array[i])) {
+      array[i] = null;
+    }
+    i++;
+  }
 }
 while (i < array.length);
-
 console.log('Проверка и изменение массива завершена');
 console.log(array);
