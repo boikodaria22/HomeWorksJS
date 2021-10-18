fa3bc6d80b3ab4544a20ee2a97625cd9cdce0dbb
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 22:14:17 +0300

    Update HomeWorkJS_3.2.js

--- JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.2.js
+++ JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.2.js
@@ -1,16 +1,13 @@
 function checkLengthArray(arrNum, arrStr) {
-
-	if (arrNum.length !== arrStr.length) {
-		return 'Массивы имеют разную длину';
-	} else {
-   		var sortNum = arrNum.sort((a, b) => {
-        	return a < b ? -1 : 1
-    		})
-   	 }
-
-	arrNum.splice(arrNum.length / 2, 0, arrStr);
-	var merged = [].concat.apply([], arrNum);
-	console.log(merged.slice(0, merged.length / 2));
+  if (arrNum.length !== arrStr.length) {
+    return 'Массивы имеют разную длину';
+  } else {
+    var sortNum = arrNum.sort((a, b) => {
+      return a < b ? -1 : 1
+    })
+  }
+  arrNum.splice(arrNum.length / 2, 0, arrStr);
+  var merged = [].concat.apply([], arrNum);
+  console.log(merged.slice(0, merged.length / 2));
 }
-
-checkLengthArray([73, 4, 11, 234, 58, 134],['js', 'css', 'jq', 'html', 'vue', 'bootstrap']);
+checkLengthArray([73, 4, 11, 234, 58, 134], ['js', 'css', 'jq', 'html', 'vue', 'bootstrap']);
