fc916283f5be3190d1844f4dccfebb3c40d1be8b
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 20:17:46 +0300

    Update HomeWorkJS_3.4.js

--- JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.4.js
+++ JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.4.js
@@ -1,11 +1,11 @@
  /************ substr **********************/
 function frazeWithoutWord(fraze, word) {
-	if (fraze.includes(word) === true) {
+	if (fraze.includes(word)) {
 		var indexWord = fraze.indexOf(word);
 		var newString = fraze.substr(0, indexWord ) + fraze.substr(indexWord + (word.length + 1));
 		return newString;
 	} else {
-		return fraze + ': '+word;
+		return `${fraze}: ${word}`;
 	}
 }
 
@@ -13,12 +13,12 @@ frazeWithoutWord('mama mila ramu','mila')
 
  /************ substring **********************/
 function frazeWithoutWord(fraze, word) {
-	if (fraze.includes(word) === true) {
+	if (fraze.includes(word)) {
 		var indexWord = fraze.indexOf(word);
 		var newString = fraze.substring(0, indexWord ) + fraze.substring(indexWord + (word.length + 1));
 		return newString;
 	} else {
-		return fraze + ': '+word;
+		return `${fraze}: ${word}`;
 	}
 }
 
@@ -26,12 +26,12 @@ frazeWithoutWord('mama mila ramu','mila')
 
  /************ slice **********************/
 function frazeWithoutWord(fraze, word) {
-	if (fraze.includes(word) === true) {
+	if (fraze.includes(word)) {
 		var indexWord = fraze.indexOf(word);
 		var newString = fraze.slice(0, indexWord ) + fraze.slice(indexWord + (word.length + 1));
 		return newString;
 	} else {
-		return fraze + ': '+word;
+		return return `${fraze}: ${word}`;
 	}
 }
 
