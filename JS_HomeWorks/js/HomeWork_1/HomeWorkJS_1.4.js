ef66744729651ea5818e04fb46bcdeede7111a23
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 22:12:34 +0300

    Update HomeWorkJS_1.4.js

--- JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.4.js
+++ JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.4.js
@@ -3,18 +3,13 @@ var wellcomeString = 'Nice to see you';
 var name = 'Daria';
 
 function wellcome(name, wellcomeString) {
-	return `${wellcomeString}, ${name}`;
-
+  return `${wellcomeString}, ${name}`;
 }
-
 wellcome(name, wellcomeString);
-
 //function expression
 var wellcomeString = 'Nice to see you';
 var name = 'Daria';
-
 var wellcome = function(name, wellcomeString) {
-	return `${wellcomeString}, ${name}`;
+  return `${wellcomeString}, ${name}`;
 };
-
 wellcome('Daria', 'Nice to see you');
