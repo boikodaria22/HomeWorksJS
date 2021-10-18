8672fb42995748a4b6bd54b9b8831414a2ab7ad7
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 22:13:57 +0300

    Update HomeWorkJS_3.1.js

--- JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.1.js
+++ JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.1.js
@@ -1,13 +1,13 @@
 function checkArrayFunc(...arguments) {
-	var arr = [];
-	for (var i = 0; i <= arguments.length; i++) {
-		if (Array.isArray(arguments[i])) {
-			arr.push(arguments[i]);
-		}
-	}
-	var merged = [].concat.apply([], arr);
-	merged.push('End');
-	merged.unshift('Start');
-	console.log(merged.reverse());
+  var arr = [];
+  for (var i = 0; i <= arguments.length; i++) {
+    if (Array.isArray(arguments[i])) {
+      arr.push(arguments[i]);
+    }
+  }
+  var merged = [].concat.apply([], arr);
+  merged.push('End');
+  merged.unshift('Start');
+  console.log(merged.reverse());
 }
 checkArrayFunc(null, 123, 'JS', ['Nastya', 'Dima', 'Max', 'Masha'], undefined, {}, true, ['Sasha', 'Denis', 'Marina']);
