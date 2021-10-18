ac6e94096464530be3658770a9c902778c8850a7
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 13:36:32 +0300

    Update HomeWorkJS_3.2.js

--- JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.2.js
+++ JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.2.js
@@ -3,25 +3,10 @@ function checkLengthArray(arrNum, arrStr) {
 	if (arrNum.length !== arrStr.length) {
 		return 'Массивы имеют разную длину';
 	} else {
-		var bubblesort = function(arrNum) {
-			var done = false;
-			while (!done) {
-				done = true;
-				
-				for (var i = 1; i < arrNum.length; i++) {
-				
-					if (arrNum[i - 1] > arrNum[i]) {
-						done = false;
-						var tmp = arrNum[i - 1];
-						arrNum[i - 1] = arrNum[i];
-						arrNum[i] = tmp;
-					}
-				}
-			}
-			return arrNum;
-		};
-		bubblesort(arrNum);
-	}
+   		var sortNum = arrNum.sort((a, b) => {
+        	return a < b ? -1 : 1
+    		})
+   	 }
 
 	arrNum.splice(arrNum.length / 2, 0, arrStr);
 	var merged = [].concat.apply([], arrNum);
