c768b4a3425a52e37bd7b2c117d1b161235da53f
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-18 22:11:22 +0300

    Update HomeWorkJS_1.2.js

--- JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.2.js
+++ JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.2.js
@@ -1,45 +1,41 @@
 // for
 var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
-
 for (var i = 0; i < array.length; i++) {
-	if (isNaN(array[i])) {
-		array[i] = +(array[i]);
-	}
-	if (!array[i] % 3 ) {
-		n++;
-	}
+  if (isNaN(array[i])) {
+    array[i] = +(array[i]);
+  }
+  if (!array[i] % 3) {
+    n++;
+  }
 }
 n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
-
 // while
 var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 var i = 0;
 while (i < array.length) {
-	if (isNaN(array[i])) {
-		array[i] = +(array[i]);
-	}
-	if (!array[i] % 3) {
-		n++;
-	}
-	i++;
+  if (isNaN(array[i])) {
+    array[i] = +(array[i]);
+  }
+  if (!array[i] % 3) {
+    n++;
+  }
+  i++;
 }
 n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
-
 //do...while
 var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 var i = 0;
-
 do {
-	if (isNaN(array[i])) {
-		array[i] = +(array[i]);
-	}
-	if (!array[i] % 3) {
-		n++;
-	}
-	i++;
+  if (isNaN(array[i])) {
+    array[i] = +(array[i]);
+  }
+  if (!array[i] % 3) {
+    n++;
+  }
+  i++;
 }
 while (i < array.length);
 n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
