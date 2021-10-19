1dde1a96b568a7bb3f43cbceccdbaa6542cf0250
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-19 17:17:23 +0300

    Update HomeWorkJS_4.2.js

--- JS_HomeWorks/js/HomeWork_4/HomeWorkJS_4.2.js
+++ JS_HomeWorks/js/HomeWork_4/HomeWorkJS_4.2.js
@@ -1,11 +1,12 @@
 var arr = [];
-function randomInteger(min=0 , max=50 ) {
+
+function randomInteger(min = 0, max = 50) {
   var num = Math.round(min - 0.5 + Math.random() * (max - min + 1));
-  if(!arr.includes (num)){
+  if (!arr.includes(num)) {
     arr.push(num);
     return arr;
-  }else{
-    return 'Это число уже есть в массиве!'}
-	}
-
+  } else {
+    return 'Это число уже есть в массиве!'
+  }
+}
 randomInteger();
