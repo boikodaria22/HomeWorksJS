6b4bbb0f760d7177f5dc63ffd24d15cc770eb46c
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-19 10:47:54 +0300

    Update HomeWorkJS_1.2.js

--- JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.2.js
+++ JS_HomeWorks/js/HomeWork_1/HomeWorkJS_1.2.js
@@ -2,12 +2,7 @@
 var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 for (var i = 0; i < array.length; i++) {
-  if (isNaN(array[i])) {
-    array[i] = +(array[i]);
-  }
-  if (array[i] % 3) {
-    n++;
-  }
+ !(array[i] % 3) ? n++: '';
 }
 n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
 // while
@@ -15,12 +10,7 @@ var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 var i = 0;
 while (i < array.length) {
-  if (isNaN(array[i])) {
-    array[i] = +(array[i]);
-  }
-  if (array[i] % 3) {
-    n++;
-  }
+  !(array[i] % 3) ? n++: '';
   i++;
 }
 n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
@@ -29,12 +19,7 @@ var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 var i = 0;
 do {
-  if (isNaN(array[i])) {
-    array[i] = +(array[i]);
-  }
-  if (array[i] % 3) {
-    n++;
-  }
+ !(array[i] % 3) ? n++: '';
   i++;
 }
 while (i < array.length);
