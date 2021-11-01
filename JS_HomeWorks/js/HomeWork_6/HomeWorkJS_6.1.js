91d673e932626efa89ae1e990c6e746b4266e959
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-01 22:17:17 +0200

    Update HomeWorkJS_6.1.js

--- JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.1.js
+++ JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.1.js
@@ -4,17 +4,12 @@ var obj = {
   setValues: function() {
     this.firstOperand = +prompt("Enter first value");
     this.secondOperand = +prompt("Enter second value");
-    if (isNaN(this.firstOperand && this.secondOperand) || !this.firstOperand || !this.secondOperand) {
-      console.log('Ошибка!Введите числа');
-      this.setValues()
-    } else {
-      console.log(`Первое число: ${ this.firstOperand}\nВторое число: ${ this.secondOperand}`)
-    }
+    this.firstOperand && this.secondOperand ? console.log(`Первое число: ${ this.firstOperand}\nВторое число: ${ this.secondOperand}`) : undefined;
   },
   sumValue: function() {
-    return this.firstOperand && this.secondOperand ? (this.firstOperand + this.secondOperand) : 'Числа не заданы';
+    return !(isNaN(this.firstOperand && this.secondOperand) || !this.firstOperand || !this.secondOperand) ? (this.firstOperand + this.secondOperand) : 'Числа не заданы';
   },
   multiplyValues: function() {
-    return this.firstOperand && this.secondOperand ? (this.firstOperand * this.secondOperand) : 'Числа не заданы';
+    return !(isNaN(this.firstOperand && this.secondOperand) || !this.firstOperand || !this.secondOperand) ? (this.firstOperand * this.secondOperand) : 'Числа не заданы';
   },
 }
