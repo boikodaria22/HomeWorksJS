48e4d147382ddcc746bb1c25b08dc823da12034f
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-01 22:40:59 +0200

    Update HomeWorkJS_6.2.js

--- JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.2.js
+++ JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.2.js
@@ -2,46 +2,27 @@ function calculatorMaker(firstOperand = 0, secondOperand = 0) {
   this.firstOperand = +firstOperand;
   this.secondOperand = +secondOperand;
   this.history = [];
+  this.checkOperand = function(firstOperand, secondOperand) {
+    return (this.firstOperand instanceof Number || typeof this.firstOperand === 'number' && !isNaN(this.firstOperand)) && (this.secondOperand instanceof Number || typeof this.secondOperand === 'number' && !isNaN(this.secondOperand)) ? true : false;
+  }
 }
-
 calculatorMaker.prototype.addition = function() {
-  if ((this.firstOperand instanceof Number || typeof this.firstOperand === 'number' && !isNaN(this.firstOperand)) && (this.secondOperand instanceof Number || typeof this.secondOperand === 'number' && !isNaN(this.secondOperand))) {
-    this.history.push(`${this.firstOperand}+${this.secondOperand}`);
-    return +(this.firstOperand + this.secondOperand);
-  } else {
-    console.log('Not a number');
-  }
+  this.checkOperand() ? this.history.push(`${this.firstOperand}+${this.secondOperand}`) : console.log('Not a number');
+  return +(this.firstOperand + this.secondOperand)
 };
-
 calculatorMaker.prototype.subtraction = function() {
-  if ((this.firstOperand instanceof Number || typeof this.firstOperand === 'number' && !isNaN(this.firstOperand)) && (this.secondOperand instanceof Number || typeof this.secondOperand === 'number' && !isNaN(this.secondOperand))) {
-    this.history.push(`${this.firstOperand}-${this.secondOperand}`);
-    return +(this.firstOperand - this.secondOperand);
-  } else {
-    console.log('Not a number');
-  }
+  this.checkOperand() ? this.history.push(`${this.firstOperand}-${this.secondOperand}`) : console.log('Not a number');
+  return +(this.firstOperand - this.secondOperand)
 };
-
 calculatorMaker.prototype.multiplication = function() {
-  if ((this.firstOperand instanceof Number || typeof this.firstOperand === 'number' && !isNaN(this.firstOperand)) && (this.secondOperand instanceof Number || typeof this.secondOperand === 'number' && !isNaN(this.secondOperand))) {
-    this.history.push(`${this.firstOperand}*${this.secondOperand}`);
-    return +(this.firstOperand * this.secondOperand);
-  } else {
-    console.log('Not a number');
-  }
+  this.checkOperand() ? this.history.push(`${this.firstOperand}*${this.secondOperand}`) : console.log('Not a number');
+  return +(this.firstOperand * this.secondOperand)
 };
-
 calculatorMaker.prototype.division = function() {
-  if ((this.firstOperand instanceof Number || typeof this.firstOperand === 'number' && !isNaN(this.firstOperand)) && (this.secondOperand instanceof Number || typeof this.secondOperand === 'number' && !isNaN(this.secondOperand))) {
-    this.history.push(`${this.firstOperand}/${this.secondOperand}`);
-    return +(this.firstOperand / this.secondOperand);
-  } else {
-    console.log('Not a number');
-  }
+  this.checkOperand() ? this.history.push(`${this.firstOperand}/${this.secondOperand}`) : console.log('Not a number');
+  return +(this.firstOperand / this.secondOperand)
 };
-
 calculatorMaker.prototype.showHistory = function() {
   !this.history.length ? console.log('Список операций – пуст') : console.log(`Список операций, выполненный этим экземпляром:\n${this.history.join('\n')}`);
 };
-
 var calculate = new calculatorMaker(8,10)
