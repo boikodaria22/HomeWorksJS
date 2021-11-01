408427cff04a1fbae26176c20a2f8ebb51b58cd4
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-01 16:24:48 +0200

    Update HomeWorkJS_6.1.js

--- JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.1.js
+++ JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.1.js
@@ -1,14 +1,14 @@
 var obj = {
   firstOperand:undefined,
   secondOperand:undefined,
-  setValues: function() {
+  setValues() {
     this.firstOperand = +prompt("Enter first value");
     this.secondOperand = +prompt("Enter second value");
   },
-  sumValues: function() {
+  sumValue() {
     return this.firstOperand&& this.secondOperand ? (this.firstOperand + this.secondOperand) : 'Числа не заданы';
   },
-  multiplyValues: function() {
+  multiplyValues() {
     return this.firstOperand && this.secondOperand ? (this.firstOperand * this.secondOperand) : 'Числа не заданы';
   },
 };
