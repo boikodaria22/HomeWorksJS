96f8a467b64186b8ee2efba69e0502846cfa3101
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-14 15:56:30 +0200

    Add files via upload

---
+++ JS_HomeWorks/js/HomeWork_8/HomeWorkJS_8.5..js
@@ -0,0 +1,56 @@
+
+  
+const rangesArray = [
+  {
+    start: '2000-01-23T04:00:00.000+00:00',
+    end: '2000-01-23T07:00:00.000+00:00'
+  },
+  {
+    start: '2000-01-23T09:00:00.000+00:00',
+    end: '2000-01-23T11:00:00.000+00:00'
+  },
+  {
+    start: '2000-01-25T04:00:00.000+00:00',
+    end: '2000-01-25T18:00:00.000+00:00'
+  },
+  {
+    start: '2000-01-27T01:00:00.000+00:00',
+    end: '2000-01-27T03:00:00.000+00:00'
+  },
+  {
+    start: '2000-01-28T03:00:00.000+00:00',
+    end: '2000-01-28T07:000+00:00'
+  },
+  {
+    start: '2000-01-28T10:00:00.000+00:00',
+    end: '2000-01-28T12:00:00.000+00:00'
+  }
+] ;
+  function vector(rangesArray) {
+
+    let getDatesFromObj=[]
+    let getTimeFromObj = []
+ 
+    getDatesFromObj.push(rangesArray.map(x => x.start.substring(0, 10)),rangesArray.map(y => y.end.substring(0, 10)));
+    let listDates = getDatesFromObj.reduce((a, b) => a.concat(b));
+    let uniqueDates =  listDates.sort().filter(function(item, pos, ary) {
+          return !pos || item != ary[pos - 1];
+      });
+      getTimeFromObj.push(rangesArray.map(x => x.start.substring(11, 13)),rangesArray.map(y => y.end.substring(11, 13)))
+      let preresArr = Array(uniqueDates.length);
+
+      for (let i = 0; i < preresArr.length; i++) {
+        preresArr[i] = `${uniqueDates[i]}:`.padEnd(24,'*');
+      }
+      
+      return preresArr;
+  }
+  
+
+
+
+
+
+  
+
+
