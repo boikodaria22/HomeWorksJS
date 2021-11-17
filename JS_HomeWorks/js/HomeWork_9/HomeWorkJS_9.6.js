0cce0ae9347c36f62f8c0744a19675f5dccc1cd4
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-17 17:04:15 +0200

    Update HomeWorkJS_9.6.js

--- JS_HomeWorks/js/HomeWork_9/HomeWorkJS_9.6.js
+++ JS_HomeWorks/js/HomeWork_9/HomeWorkJS_9.6.js
@@ -1,7 +1,9 @@
+
 function sumNumbers(arrayOfNum) {
   let value = 0
   let arr = []
-  return arrayOfNum.forEach(item => {
+  arrayOfNum.forEach(item => {
     arr.push(value += item)
   })
+return arr
 }
