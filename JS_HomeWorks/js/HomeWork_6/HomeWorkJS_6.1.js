a758c8c14d77d7daaa34c3bf5879f5ea85053c4d
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-17 22:48:13 +0200

    Update HomeWorkJS_6.1.js

--- JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.1.js
+++ JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.1.js
@@ -1,10 +1,10 @@
 var obj = {
-  firstOperand: undefined,
-  secondOperand: undefined,
+  firstOperand: null,
+  secondOperand: null,
   setValues: function() {
     this.firstOperand = +prompt("Введите первое число");
     this.secondOperand = +prompt("Введите второе число");
-    this.firstOperand && this.secondOperand ? console.log(`Первое число: ${ this.firstOperand}\nВторое число: ${ this.secondOperand}`) : undefined;
+    this.firstOperand && this.secondOperand ? console.log(`Первое число: ${ this.firstOperand}\nВторое число: ${ this.secondOperand}`) : null;
   },
   sumValue: function() {
     return !(isNaN(this.firstOperand && this.secondOperand) || !this.firstOperand || !this.secondOperand) ? (this.firstOperand + this.secondOperand) : 'Числа не заданы';
