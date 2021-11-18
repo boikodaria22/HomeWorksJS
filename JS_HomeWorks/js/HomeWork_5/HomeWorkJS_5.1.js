10867d5071f176bfa90f5afbbddaa5221a2dce59
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-18 12:12:47 +0200

    Update HomeWorkJS_5.1.js

--- JS_HomeWorks/js/HomeWork_5/HomeWorkJS_5.1.js
+++ JS_HomeWorks/js/HomeWork_5/HomeWorkJS_5.1.js
@@ -2,7 +2,7 @@ var perol = '12345678'
 
 var checkPassword = ((function(password) {
   return function(str) {
-    return str === password ? true: false;
+    return str === password
   };
 })(parol));
 checkPassword('12345678');
