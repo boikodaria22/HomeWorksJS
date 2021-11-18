46fc31817001a4bc243a838cc15a3c0ba35f86eb
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-18 15:14:30 +0200

    Update HomeWorkJS_6.2.js

--- JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.2.js
+++ JS_HomeWorks/js/HomeWork_6/HomeWorkJS_6.2.js
@@ -2,41 +2,23 @@ function calculatorMaker(firstOperand = 0, secondOperand = 0) {
    this.firstOperand = +firstOperand;
    this.secondOperand = +secondOperand
    this.history = [];
-   this.checkOperand = function(firstOperand, secondOperand) {
-      return (this.firstOperand instanceof Number || typeof this.firstOperand === 'number' && !isNaN(this.firstOperand)) && (this.secondOperand instanceof Number || typeof this.secondOperand === 'number' && !isNaN(this.secondOperand)) && (this.firstOperand !== 0 && this.secondOperand !== 0) ? true : false;
-   }
  }
 calculatorMaker.prototype.addition = function() {
-   if (this.checkOperand()) {
      this.history.push(`${this.firstOperand}+${this.secondOperand}`);
      return +(this.firstOperand + this.secondOperand)
-   } else {
-     console.log('Not a number');
-   }
  };
  calculatorMaker.prototype.subtraction = function() {
-   if (this.checkOperand()) {
      this.history.push(`${this.firstOperand}-${this.secondOperand}`);
      return +(this.firstOperand - this.secondOperand)
-   } else {
-     console.log('Not a number');
-   }
  };
  calculatorMaker.prototype.multiplication = function() {
-   if (this.checkOperand()) {
      this.history.push(`${this.firstOperand}*${this.secondOperand}`);
      return +(this.firstOperand * this.secondOperand)
-   } else {
-     console.log('Not a number');
-   }
+
  };
  calculatorMaker.prototype.division = function() {
-   if (this.checkOperand()) {
      this.history.push(`${this.firstOperand}/${this.secondOperand}`);
      return +(this.firstOperand / this.secondOperand)
-   } else {
-     console.log('Not a number');
-   }
  };
  calculatorMaker.prototype.showHistory = function() {
    !this.history.length ? console.log('Список операций – пуст') : console.log(`Список операций, выполненный этим экземпляром:\n${this.history.join('\n')}`);
