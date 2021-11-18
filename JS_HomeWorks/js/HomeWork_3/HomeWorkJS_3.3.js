e56c805048ec6396848818c68976cea6be571f0f
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-18 12:05:57 +0200

    Update HomeWorkJS_3.3.js

--- JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.3.js
+++ JS_HomeWorks/js/HomeWork_3/HomeWorkJS_3.3.js
@@ -1,11 +1,10 @@
 function clipString(str, n) {
-  var l = str.length;
-  if (l < n) {
+  if (str.length < n) {
     var strRe = str.replace(/ /g, "-");
     var strFirst = strRe[0].toUpperCase() + strRe.slice(1);
     var newStr = strFirst.slice(0, strFirst.length - 1) + strFirst[strFirst.length - 1].toUpperCase();
     return newStr;
-  } else if (l > n) {
+  } else if (str.length > n) {
     var strN = str.slice(0, n);
     return strN.concat('...');
   }
