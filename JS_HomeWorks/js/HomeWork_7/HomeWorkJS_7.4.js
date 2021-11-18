e256d1fe333548231dc1448e08a816d886b179d7
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-18 17:21:49 +0200

    Update HomeWorkJS_7.4.js

--- JS_HomeWorks/js/HomeWork_7/HomeWorkJS_7.4.js
+++ JS_HomeWorks/js/HomeWork_7/HomeWorkJS_7.4.js
@@ -4,6 +4,6 @@ ProductCreator.prototype.showProductCreatorInstanceCounter = function() {
   ProductCreator.prototype.instanceCounter++;
   if (this instanceof ProductCreator) {
     this.id = ProductCreator.prototype.instanceCounter;
-    console.log(`Это экземпляр имеет id = ${this.id}`);
+    return (this.id)
   }
 };
