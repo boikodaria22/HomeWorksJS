9f414fedcac69a51a612c7e799d6ea9bd168c1b3
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-18 12:32:48 +0200

    Update HomeWorkJS_5.5.js

--- JS_HomeWorks/js/HomeWork_5/HomeWorkJS_5.5.js
+++ JS_HomeWorks/js/HomeWork_5/HomeWorkJS_5.5.js
@@ -1,9 +1,15 @@
-function rangeFunc(start, end) {
-  var res = [];
-  if (start === end) {
-    return res;
-  }
-  res.push(start);
-  return (res.concat(rangeFunc(start + 1, end))).toString();
+function rangeFunc(start, end, step) {
+  if (start > end) {
+         return 'Ошибка! Start не должен быть больше End'
+   }
+            step = step ? step : ''
+            if (start < end) {
+                step += start++ + ', '
+                return rangeFunc(start, end, step)
+            }
+
+            return step + end
 }
 rangeFunc(1, 5);
+
+
