bce158d797f2fa5df4390e12f7e113a61e20fcd2
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-18 13:28:04 +0200

    Update HomeWorkJS_8.1.js

--- JS_HomeWorks/js/HomeWork_8/HomeWorkJS_8.1.js
+++ JS_HomeWorks/js/HomeWork_8/HomeWorkJS_8.1.js
@@ -1,8 +1,15 @@
-for (let i = 1; i <= 20; i++) { 
-  setTimeout(function() {
-      console.log(i)
-    }, 1000 * i)
+
+function counter() {
+  let i = 1
+  setTimeout(function func() {
+    console.log(i)
+    if (i < 20) {
+      setTimeout(func, 1000)
+    }
+    i++
+  }, 1000) 
 }
+function counter(){
 let i = 1;
 let timer = setInterval(function() {
     console.log(i);
@@ -11,3 +18,5 @@ let timer = setInterval(function() {
     }
     i++;
   }, 1000)
+}
+
