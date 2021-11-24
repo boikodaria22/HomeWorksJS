166e8eaa2e6a32fa422fd77a8429174068c04d5d
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-11-24 20:35:31 +0200

    Update HomeWorkJS_7.4.js

--- JS_HomeWorks/js/HomeWork_7/HomeWorkJS_7.4.js
+++ JS_HomeWorks/js/HomeWork_7/HomeWorkJS_7.4.js
@@ -1,6 +1,6 @@
 function ProductCreator() {
-  this.instanceCounter = ++count
-  this.id = `Имеет id:${this.instanceCounter}`
 }  
 let count = 0
+ProductCreator.prototype.instanceCounter = ++count
+ProductCreator.prototype.id = `Имеет id:${ProductCreator.prototype.instanceCounter}`
 ProductCreator.prototype.showProductCreatorInstanceCounter =(()=>count)
