e98b55ae2f3877fb61f0a970112dc4a9a718bf80
Daria <shabalinadaria2208@gmail.com>
2021-12-14 10:28:50 +0200

    16->15

--- JS_HomeWorks/js/HomeWork_9/HomeWorkJS_9.1.js
+++ JS_HomeWorks/js/HomeWork_9/HomeWorkJS_9.1.js
@@ -2,4 +2,4 @@ function sumOfSquares(array) {
   const sumEvens = arr => arr.filter(even => !(even % 2)).map(square => Math.sqrt(square)).reduce((a, b) => a + b);
   console.log(sumEvens(array));
 }
-sumOfSquares([1, 3, 4, 7, 16])
+sumOfSquares([1, 3, 4, 7, 15])
